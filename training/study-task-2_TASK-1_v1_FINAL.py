04c1e4e552458ad45e87727558f4abc5471ab75c
Fiddy Bux <fiddybux@gmail.com>
2020-05-17 23:22:04 +0100

    Update study-task-2_TASK-1_v1_FINAL.py

--- training/study-task-2_TASK-1_v1_FINAL.py
+++ training/study-task-2_TASK-1_v1_FINAL.py
@@ -1,10 +1,4 @@
-#region Main "Title"
 # coding=utf-8
-"""Study Task 2 -Task 1
-Author: Russell Dyson
-Custom region folding: "Visual Studio Code Custom Folding Extension" (maptz)
-"""
-#endregion
 #region Main "Setup Environment"
 import os
 os.system('cls')
